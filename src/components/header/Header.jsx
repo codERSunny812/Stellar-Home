cbcf5edc3b4dd63a90b2def8752d236e7c096d47
codersunny812 <sengersunny448@gmail.com>
2024-12-20 09:10:38 +0530

    Header of the real state is created successfully

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,22 @@
 
-import Logo from '../../assets/Logo.png'
+import Logo from '../../assets/site.png'
 import './Header.css'
 
 const Header = () => {
   return (
     <section className='h-wrapper'>
-        <div className="h-container">
-            <img src={Logo} alt="" width={130}/>
+          <div className="h-container flexCenter  innerWidth ">
+            <img src={Logo} alt="" width={110}/>
 
-            <div className="h-menu">
-                <ul>
+            <div className="h-menu flexCenter paddings  ">
+                <ul className='flexCenter '>
                     <li>residence</li>
                     <li>our values</li>
                     <li>contact us</li>
                     <li>get started</li>
                 </ul>
-                <button type="button">
-                    <button>contact</button>
+                <button type="button" className='button'>
+                    contact
                 </button>
             </div>
         </div>
