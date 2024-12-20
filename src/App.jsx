e8488c116e2a174801cac7dd12f88375fd6bbd3d
codersunny812 <sengersunny448@gmail.com>
2024-12-20 23:24:17 +0530

    cards created and swiper added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Header from './components/header/Header'
 import Hero from './components/Hero/Hero'
-
+import './App.css'
+import Company from './components/Companies/Company'
+import Residence from './components/Residence/Residence'
 function App() {
 
 
   return (
    <>
    <div className="app">
-        <Header />
-        <Hero />
+    <div className="">
+      <div className="white-gradient"/>
+          <Header />
+          <Hero />
+    </div>
+    <Company/>
+    <Residence/>
+
    </div>
  
    </>
---
+++ src/components/Residence/Residence.jsx
@@ -0,0 +1,63 @@
+import "./residence.css";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination"; 
+import { data } from "../../utils/Carasoul";
+import {sliderSetting} from '../../utils/Common'
+
+const Residence = () => {
+  return (
+    <section className="residence-wrapper">
+      <div className="paddings innerWidth residence-container">
+        {/* heading */}
+        <div className="residence-heading flexColStart paddings">
+          <span className="orangeText">best choice</span>
+          <span className="primaryText ">popular residence</span>
+        </div>
+
+        {/* Carousel Start */}
+        <Swiper {...sliderSetting} >
+            <SliderButton/>
+          {data?.map(({ name, price, detail, image }, i) => {
+            return (
+              <SwiperSlide key={i}>
+                <div className=" flexColStart  residence-card">
+                  <img src={image} alt={name} />
+
+                    <span className="secondaryText residence-price">
+                      <span style={{ color: "orange" }}>$</span>
+                      <span>{price}</span>
+                    </span>
+
+                    <span className="primaryText residence-name">{name}</span>
+                    <span className="secondaryText residence-detail">
+                      {detail}
+                    </span>
+
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Residence;
+
+
+const SliderButton =()=>{
+    const swiper = useSwiper()
+    return(
+        <div className="residence-button">
+        <button onClick={()=> swiper.slidePrev()}>
+            &lt;
+        </button>
+            <button onClick={() => swiper.slideNext()} > 
+          &gt;
+        </button>
+        </div>
+    )
+}
