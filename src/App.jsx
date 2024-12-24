cd93f4ba082831fa3acb5898c8f12648b6986cb2
codersunny812 <sengersunny448@gmail.com>
2024-12-24 20:32:33 +0530

    app file bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import Header from './components/header/Header'
-import Hero from './components/Hero/Hero'
-import './App.css'
-import Company from './components/Companies/Company'
-import Residence from './components/Residence/Residence'
-import Value from './components/Value/Value'
-import Contact from './components/Contact/Contact'
-import GetStarted from './components/getstarted/GetStarted'
-import Footer from './components/footer/footer'
+import Header from "./components/header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Company from "./components/Companies/Company";
+import Residence from "./components/Residence/Residence";
+import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/getstarted/GetStarted";
+import Footer from "./components/footer/footer";
 function App() {
-
-
   return (
-   <>
-   <div className="app">
-    <div>
-      <div className="white-gradient"/>
+    <>
+      <div className="app">
+        <div>
+          <div className="white-gradient" />
           <Header />
           <Hero />
-    </div>
-    <Company />
-    <Residence/>
-    
-    <Value/>
-    <Contact/>
-    <GetStarted/>
-    <Footer/>
+        </div>
+        <Company />
+        <Residence />
 
-   </div>
- 
-   </>
-  )
+        <Value />
+        <Contact />
+        <GetStarted />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
